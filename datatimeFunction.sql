63b4c45289a9f096d94676332a426bade5d85ca0
MD TOMAS UDDIN <143870012+MDTOMSDUDDIN@users.noreply.github.com>
2024-11-19 21:54:24 +0600

    arragation/datetimeFunction

---
+++ datatimeFunction.sql
@@ -0,0 +1,14 @@
+-- Active: 1731564887736@@127.0.0.1@3306@demo
+SELECT NOW() as col;
+
+SELECT CURDATE() as col;
+
+SELECT CURTIME() as col;
+
+SELECT YEAR("2024-10-01") as col;
+
+SELECT MONTH("2024-10-01") as col;
+
+SELECT DAY("2024-10-01") as col;
+
+
